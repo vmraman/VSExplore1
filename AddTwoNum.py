df0d89be3b1b276fd553f73d5f830817fe83a966
vmraman <Vidhya.Raman@Lumen.com>
2024-01-10 14:56:46 +0530

    Adding_Two_Numbers

---
+++ AddTwoNum.py
@@ -0,0 +1,4 @@
+#This programs adds two numbers.
+num1 = int(input("Enter a whole number--> "))
+num2 = int(input("Enter another whole number--> "))
+print("Addition of two numbers  = ", num1 + num2)
